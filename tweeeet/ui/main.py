a4b39316bd6d4a11db93112a839ea031a120d605
Mike <mikeandmore@gmail.com>
2011-01-27 19:50:56 +0800

    add tray

--- tweeeet/ui/main.py
+++ tweeeet/ui/main.py
@@ -39,6 +39,8 @@ class MainWindow(object):
         self.widgets['editor_hbox'].pack_start(self.editor.view)
         self.builder.connect_signals(self, None)
 
+        self.settings = Settings()
+
         Pipeline().set_notification_callback(self.show_status)
 
     def show_status(self, status, message):
@@ -48,24 +50,39 @@ class MainWindow(object):
         status_image.set_pixel_size(16)
         self.widgets['statusbar'].push(0, message)
 
+    def on_tray_popup_menu(self, icon, evt_btn, evt_time):
+        menu = self.widgets['tray_menu']
+        menu.popup(None, None, gtk.status_icon_position_menu, evt_btn, evt_time, icon)
+
+    def on_tray_activate(self, *args, **kwargs):
+        if self.shown:
+            self.settings['window-x'], self.settings['window-y'] = self.window.get_position()
+            self.window.hide_on_delete()
+            self.shown = False
+        else:
+            self.window.present()
+            self.window.move(self.settings['window-x'], self.settings['window-y'])
+            self.shown = True
+        return True
+
     def on_send_clicked(self, widget):
         self.editor.on_send()
 
     def on_pref_clicked(self, widget):
-        settings = Settings()
-        self.widgets['id_entry'].set_text(settings['username'])
-        self.widgets['password_entry'].set_text(settings['password'])
+        self.widgets['id_entry'].set_text(self.settings['username'])
+        self.widgets['password_entry'].set_text(self.settings['password'])
         dlg = self.widgets['pref_dialog']
         dlg.run()
         dlg.hide()
         # update the settings
-        settings['username'] = self.widgets['id_entry'].get_text()
-        settings['password'] = self.widgets['password_entry'].get_text()
+        self.settings['username'] = self.widgets['id_entry'].get_text()
+        self.settings['password'] = self.widgets['password_entry'].get_text()
 
     def on_editor_key_press(self, widget, event):
         self.editor.update_state(0)
 
-    def on_quit(self, widget, event):
+    def on_quit(self, *args, **kwargs):
+        self.settings['window-x'], self.settings['window-y'] = self.window.get_position()
         gtk.main_quit()
 
     def on_refresh_clicked(self, widget):
@@ -75,5 +92,7 @@ class MainWindow(object):
         return self.tab_man
 
     def show(self):
+        self.shown = True
         self.window.show_all()
+        self.window.move(self.settings['window-x'], self.settings['window-y'])
         self.editor.view.grab_focus()
